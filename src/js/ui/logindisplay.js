558fb59f0dce23e4b7b037fbc44f4b5e2f0c0853
TaranJoh <tarjoh51636@stud.noroff.no>
2024-05-19 17:52:33 +0200

    fixes issues with responsiveness

--- src/js/ui/logindisplay.js
+++ src/js/ui/logindisplay.js
@@ -23,7 +23,7 @@ export function handleNoUserForPostCreation() {
     <div class="d-flex align-items-center justify-content-center">
       <a href="/login.html" class="text-nowrap text-uppercase text-decoration-none">Log in</a>
       <a href="/signup.html"
-        ><button class="btn btn-custom ms-3 text-nowrap text-white text-uppercase rounded-pill p-4 pt-2 pb-2">Create account</button></a
+        class="btn btn-custom ms-3 text-nowrap text-white text-uppercase rounded-pill p-4 pt-2 pb-2">Create account</a
       >
     </div>`;
   }
